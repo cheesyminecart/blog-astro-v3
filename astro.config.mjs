8e850dcabc48ac1d52bf11b5f3141fe42e2fd102
Julien Cheung <129882147+cheesyminecart@users.noreply.github.com>
2023-08-01 21:13:47 +0800

    IM DONT LIKE YOU

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server'
   site: "https://astro-paper.pages.dev/", // replace this with your deployed domain
   integrations: [
     tailwind({
