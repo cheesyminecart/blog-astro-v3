4330e4f0a56dc531ea26ae130e9ea9fe96a8bbe8
Julien Cheung <129882147+cheesyminecart@users.noreply.github.com>
2024-05-15 22:51:47 +0800

    redo

---
+++ astro.config.mjs
@@ -0,0 +1,42 @@
+import { defineConfig } from "astro/config";
+import react from '@astrojs/react';
+import tailwind from "@astrojs/tailwind";
+import remarkToc from "remark-toc";
+import remarkCollapse from "remark-collapse";
+import sitemap from "@astrojs/sitemap";
+import { remarkReadingTime } from "./src/utils/remark-reading-time.mjs";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://thespacer-blog.com", // replace this with your deployed domain
+  integrations: [
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+    react(),
+    sitemap(),
+  ],
+  markdown: {
+    remarkPlugins: [
+      remarkToc,
+      [
+        remarkCollapse,
+        {
+          test: "Table of contents",
+        },
+      ],
+    ],
+    shikiConfig: {
+      theme: "one-dark-pro",
+      wrap: true,
+    },
+    extendDefaultPlugins: true,
+  },
+  vite: {
+    optimizeDeps: {
+      exclude: ["@resvg/resvg-js"],
+    },
+  },
+});
---
+++ src/content/blog/footer_copyable.md
@@ -0,0 +1,12 @@
+---
+author: Julien
+pubDatetime: 2022-12-28T04:59:04.866Z
+title: footer copyable
+postSlug: subscribe-footer
+featured: false
+draft: true
+ogImage: ""
+description: copy this chunk of text into the bottom of each blog post
+---
+
+_Like what you see? Feel free to [subscribe](https://thespacer-blog.netlify.app/subscribe/) to our blog to receive updates whenever we post._
