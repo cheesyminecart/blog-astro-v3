cb62e6d35ab367e02027bf238aced488f84c7460
Julien Cheung <129882147+cheesyminecart@users.noreply.github.com>
2024-06-04 11:49:59 +0800

    remove vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,6 @@ import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
 import { remarkReadingTime } from "./src/utils/remark-reading-time.mjs";
 
-import vercel from "@astrojs/vercel/serverless";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://thespacer-blog.com",
@@ -32,6 +30,4 @@ export default defineConfig({
       exclude: ["@resvg/resvg-js"]
     }
   },
-  output: "server",
-  adapter: vercel()
 });
