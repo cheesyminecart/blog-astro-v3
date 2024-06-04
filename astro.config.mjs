2ca294271855317f2ef233ca446fc4b464bb5f4e
Julien Cheung <129882147+cheesyminecart@users.noreply.github.com>
2024-06-04 15:19:01 +0800

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -23,7 +23,7 @@ export default defineConfig({
       theme: "one-dark-pro",
       wrap: true
     },
-    extendDefaultPlugins: true
+    extendDefaultPlugins: true,
   },
   vite: {
     optimizeDeps: {
